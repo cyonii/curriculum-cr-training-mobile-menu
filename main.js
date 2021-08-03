2664e6e69b612124869363019acba6030423132c
CY Kalu <cy23@tutanota.com>
2021-08-03 10:42:20 +0000

    Add mobile menu functionality

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuBar = document.querySelector('.menu-bars');
+
+menuBar.addEventListener('click', () => {
+  console.log('menu bar clicked');
+  document.querySelector('.nav-main').classList.toggle('open');
+  document.querySelector('.top-header').classList.toggle('stick');
+});
